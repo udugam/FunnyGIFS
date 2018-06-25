ac23a55253b72372d28289009819e70e8749bf16
udugam <ryan.udugam@gmail.com>
2018-06-24 23:02:02 -0400

    added freeze and unfreeze functionality to gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,21 +13,43 @@ var utilities = {
             url: queryUrl,
             method: 'GET'
         }).then(function(response) {
-            utilities.renderResults(response)
+            utilities.renderResults(response,".searchResults")
         }) 
     },
-    renderResults: function(response) {
-        console.log(response);
-        //store response.data in an array then iterate through the array
-        $(".searchResults").append(response.data[0].type)
+    renderResults: function(response,divName) {
+        var responseArray = response.data
+        responseArray.forEach(function(element) {
+            var image = $("<img>");
+            image.addClass("gif");
+            image.attr("data-still", element.images.fixed_height_still.url)
+            image.attr("src", element.images.fixed_height_still.url)
+            image.attr("data-animate", element.images.fixed_height.url)
+            image.attr("data-state", "still")
+            $(divName).append(image)
+        })
+    },
+    renderButtons: function(array, divName) {
+        $(divName).empty();
+        array.forEach(function(element) {
+            var button = $("<button>");
+            button.addClass("savedGifs");
+            button.text(element);
+            $(divName).append(button)
+        })
+    },
+    newTopic: function(topicName,topicsArray) {
+        topicsArray.push(topicName);
+    },
+    unfreeze: function(image) {
+        $(image).attr("src", $(image).attr("data-animate"));
+        $(image).attr("data-state", "animate");
+    },
+    freeze: function(image) {
+        $(image).attr("src", $(image).attr("data-still"));
+        $(image).attr("data-state", "still");
     }
-
 }
 
-//Events
-$("#search").on("click", function() {
-    var input = $("#user-input").val();
-    utilities.search(input,1);
-})
+
 
 
